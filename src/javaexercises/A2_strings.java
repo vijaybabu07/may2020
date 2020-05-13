51f5935d4141c8aa31b4f9f968ca03dbb11834fa
vijaybabu07 <vijay.vj7@gmail.com>
2020-05-13 11:23:57 +0530

    commit_13May2020_11:23AM

---
+++ src/javaexercises/A2_strings.java
@@ -0,0 +1,13 @@
+package javaexercises;
+
+public class A2_strings {
+
+	public static void main(String[] args) {
+		String r1="vijay";
+		String r2="babu";
+		System.out.println(r1.concat(r2));
+		System.out.println(r1.replace('i', 'b'));
+
+	}
+
+}
